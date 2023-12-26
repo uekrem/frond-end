8caefbdd677c2a8f98b32fb7df83768712cda4e9
H. Utku Ekrem <110349452+uekrem@users.noreply.github.com>
2023-12-26 13:12:55 +0300

    Add files via upload

---
+++ SITE/pop-up.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    document.querySelector('.popup').style.display = 'flex';
+    document.querySelector('.overlay').style.display = 'block';
+}
+
+function closePopup() {
+    document.querySelector('.popup').style.display = 'none';
+    document.querySelector('.overlay').style.display = 'none';
+}
