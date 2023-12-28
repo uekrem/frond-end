0aa2693eb00e189f1b776c0ee83863b1b068a6ac
H. Utku Ekrem <110349452+uekrem@users.noreply.github.com>
2023-12-28 10:45:35 +0300

    Delete SITE directory

--- SITE/pop-up.js
+++
@@ -1,9 +0,0 @@
-function openPopup() {
-    document.querySelector('.popup').style.display = 'flex';
-    document.querySelector('.overlay').style.display = 'block';
-}
-
-function closePopup() {
-    document.querySelector('.popup').style.display = 'none';
-    document.querySelector('.overlay').style.display = 'none';
-}
